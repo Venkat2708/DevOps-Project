904be9a10c17dec4b22952444850d45b5bd87d93
Venkat2708 <100748292+Venkat2708@users.noreply.github.com>
2026-06-25 08:06:21 +0530

    Update config.py

--- Docker Projects/HospitalManagementSystem/config.py
+++ Docker Projects/HospitalManagementSystem/config.py
@@ -1,1 +1,18 @@
-dd
+import os
+
+class Config:
+
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        "DATABASE_URL",
+        "postgresql://admin:admin123@postgres:5432/hospitaldb"
+    )
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    REDIS_HOST = os.getenv("REDIS_HOST", "redis")
+    REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
+
+    SECRET_KEY = os.getenv(
+        "SECRET_KEY",
+        "hospital-secret-key"
+    )
